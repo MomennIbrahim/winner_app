6439d55b341f4a08ce2f0cda0d90e092386ca806
MomennIbrahim <ibrahimmomen179@gmail.com>
2024-07-14 00:31:59 +0300

    - fake push

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:winner_app/core/routing/app_router.dart';
 import 'package:winner_app/winner_app.dart';
 
 void main() {
+  //fake push
   runApp(WinnerApp(
     appRouter: AppRouter(),
   ));
